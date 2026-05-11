94565ef6a3f3437374bdfcddefa956d899c9bad8
Darisky <Dariskya99@gmail.com>
2026-05-11 19:50:35 +0700

    Dependencies adjustment

--- src/test/java/com/darisky/apitest/stepdefs/ApiTestStepdefs.java
+++ src/test/java/com/darisky/apitest/stepdefs/ApiTestStepdefs.java
@@ -39,7 +39,8 @@ public class ApiTestStepdefs {
 
     @Then("the user is successfully created and an ID is returned")
     public void theUserIsSuccessfullyCreatedAndAnIDIsReturned() {
-        theResponse.then().assertThat().statusCode(201);
+        theResponse.then().log().all()
+                .assertThat().statusCode(201);
 
         newUserId = theResponse.jsonPath().getString("id");
         System.out.println("<------SUCCESS: User created with ID: " + newUserId + "------>");
